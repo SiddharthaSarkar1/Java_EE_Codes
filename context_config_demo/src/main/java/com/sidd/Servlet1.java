ab0689a220e7ec9bf067f862f8e2d4f7e09a3602
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2023-03-21 23:42:53 +0530

    Context Config

---
+++ context_config_demo/src/main/java/com/sidd/Servlet1.java
@@ -0,0 +1,46 @@
+package com.sidd;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+//@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ServletConfig config = getServletConfig();
+		PrintWriter out = response.getWriter();
+		out.print("<h1>Accessing config parameters in Servlet1</h1>");
+		out.print("<h1>SQL = "+ config.getInitParameter("sql"));
+		out.print("<br/> Message = "+ config.getInitParameter("message")+"</h1>");
+		
+		out.print("<h1>Accessing context parameters in Servlet1</h1>");
+		ServletContext application = getServletContext();
+		out.print("<h1>My Mobile = "+ application.getInitParameter("mymobile"));
+		out.print("<br/> Food = "+ application.getInitParameter("food")+"</h1>");
+	}
+
+}
