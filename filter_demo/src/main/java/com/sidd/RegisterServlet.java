6135f82a6b782d31fc8defd6c3aea387114e0536
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2023-03-23 01:59:58 +0530

    Filters Demo

---
+++ filter_demo/src/main/java/com/sidd/RegisterServlet.java
@@ -0,0 +1,36 @@
+package com.sidd;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegisterServlet
+ */
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegisterServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		System.out.println("Hello from Servlet");
+		out.println("<h3>PAN number "+ request.getAttribute("pan") +" registered successfully. </h3>");
+		System.out.println("Bye from Servlet");
+	}
+
+}
